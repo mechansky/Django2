18d9b075c5a7cea308c4f627920a2e006107311f
Mechansky <alexmaratovich@mail.ru>
2021-11-28 17:13:48 +0300

    add homework4

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -4,15 +4,16 @@ import requests
 from social_core.exceptions import AuthForbidden
 
 from authapp.models import ShopUserProfile
+from django.conf import settings
 
 
-def save_user_profile(backend, user, response, *args, **kwargs):
+def save_user_profile(backend, user, response, settings=None, *args, **kwargs):
     if backend.name != 'vk-oauth2':
         return
 
-    url_method = 'https://apivk.com/method/'
+    url_method = 'https://api.vk.com/method/'
     access_token = response.get('access_token')
-    fields = ','.join(['bdate', 'sex', 'about'])
+    fields = ','.join(['bdate', 'sex', 'about', 'photo_max_orig'])
     api_url = f'{url_method}users.get?fields={fields}&access_token={access_token}&v=5.131'
 
     response = requests.get(api_url)
@@ -44,5 +45,13 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if 'about' in data_json:
         user.shopuserprofile.about = data_json['about']
 
+    if 'photo_max_orig' in data_json:
+        photo_path = f'users_avatars/{user.pk}.jpeg'
+        photo_full_path = f'{settings.MEDIA_ROOT}/{photo_path}'
+        photo_data = requests.get(data_json['photo_max_orig'])
+        with open(photo_full_path, 'wb') as photo_file:
+            photo_file.write(photo_data.content)
+        #user.avatar_url = data_json['photo_max_orig']
+        user_avatar = photo_path
     user.save()
 
